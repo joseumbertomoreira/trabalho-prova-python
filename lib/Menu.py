24d2db44fb35381b1eadebb704dda18083da0109
José Umberto <jose@localhost.localdomain>
2021-05-29 14:57:07 -0300

    Script principal Main; diretório lib com a classe Menu

---
+++ lib/Menu.py
@@ -0,0 +1,45 @@
+class Menu:
+  
+  Tarefas = []
+
+  def init(self):
+    
+    while True:
+      print("O que gostaria de fazer? Digite: ")
+      print("\t1 - Adicionar uma tarefa.")
+      print("\t2 - Listar as tarefas.")
+      print("\t3 - Concluir uma tarefa.")
+      print("\t4 - Buscar tarefas na base de dados.")
+      print("\t5 - Inserir tarefas na base de dados.")
+      print("\t0 - Sair.")
+
+      opcoes = int(input())
+
+      if opcoes == 1:
+        print("Adiciona tarefas")
+        print("\n")
+      
+      elif opcoes == 2:
+        print("Lista tarefas")
+        print("\n")
+      
+      elif opcoes == 3:
+        print("Concluo tarefas")
+        print("\n")
+      
+      elif opcoes == 4:
+        print("Buscar na base de dados")
+        print("\n")
+      
+      elif opcoes == 5:
+        print("Inserir na base de dados")
+        print("\n")
+      
+      elif opcoes == 0:
+        print("Bye bye!")
+        print("\n")
+        break
+
+      else:
+        print("Essa opção não existe")
+        print("\n")
