e770edb212bcb71de3d7fc57d6ae68c7497ad054
José Umberto <joseumbertomoreira@github.com>
2021-06-04 22:17:48 -0300

    As funções que permitem a interação com o banco foram adicionadas

--- lib/Crud.py
+++ lib/Crud.py
@@ -1,4 +1,5 @@
 from lib.Tarefa import Tarefa
+import sqlite3
 
 class Crud:
 
@@ -20,10 +21,27 @@ class Crud:
       else:
         return opcoes
     
+
+  def concluiTarefa(self, listaTarefas):
+
+    if(len(listaTarefas) <= 0):
+      print("Não há tarefas para concluir")
+      return
     
+    while True:
+      nome = input("Qual o nome da tarefa que gostaria de concluir? ")
+
+      for tarefa in listaTarefas:
+        if nome == tarefa.nome:
+          tarefa.concluido = 1
+          return
+      
+      print("Não existe tarefa com esse nome! Tente novamente...")
+      print("\n")
+
 
   
-  def adicionarTarefa(self, listaTarefas):
+  def adicionaTarefa(self, listaTarefas):
 
     nomeAtividade = input("Qual o nome da atividade? ")
 
@@ -36,4 +54,77 @@ class Crud:
     tarefa = Tarefa(nomeAtividade, tipoAtividade, descricaoAtividade, prazo)
     listaTarefas.append(tarefa)
 
+  
+  def mostraTarefas(self, listaTarefas):
+
+    if(len(listaTarefas) <= 0):
+      print("Não há tarefas para mostrar")
+      return
     
+    print("\n")
+
+    for tarefa in listaTarefas:
+      print(
+        f"| {tarefa.nome} | {tarefa.atividade} | {tarefa.tipo.value} | {tarefa.atividade} | {tarefa.prazo} | {tarefa.concluido} |"
+      )
+    
+    print("\n")
+
+  
+  def notInListaTarefas(self, listaTarefas, rowNome):
+
+    for tarefa in listaTarefas:
+      if tarefa.nome == rowNome:
+        print(f"{tarefa.nome} e {rowNome}")
+        
+        return False
+    
+    return True
+
+  def buscaDadosSQLite(self, listaTarefas):
+
+    conn = sqlite3.connect("./database/tarefas-db")
+
+    cursor = conn.cursor()
+
+    if not listaTarefas:
+      for row in cursor.execute("SELECT * FROM Tarefas"):
+        
+        tarefa = Tarefa(row[0], row[1], row[2], row[3])
+        listaTarefas.append(tarefa)        
+
+    else:
+      for row in cursor.execute("SELECT * FROM Tarefas"):
+
+        if self.notInListaTarefas(listaTarefas, row[0]):
+          tarefa = Tarefa(row[0], row[1], row[2], row[3])
+          listaTarefas.append(tarefa)
+
+    
+    conn.commit()
+    conn.close()
+    
+    
+
+  def insereDadosSQLite(self, listaTarefas):
+
+    if(len(listaTarefas) <= 0):
+      print("Não há tarefas para concluir")
+      return
+
+    conn = sqlite3.connect("./database/tarefas-db")
+    cursor = conn.cursor()
+
+    for tarefa in listaTarefas:
+      cursor.execute(
+        f"INSERT OR REPLACE INTO Tarefas VALUES (\
+          '{tarefa.nome}',\
+          '{tarefa.tipo.value}',\
+          '{tarefa.atividade}',\
+          '{tarefa.prazo}',\
+          {tarefa.concluido})"
+      )
+    
+    conn.commit()
+    cursor.close()    
+    conn.close()
--- lib/Menu.py
+++ lib/Menu.py
@@ -10,7 +10,7 @@ class Menu:
     while True:
       print("O que gostaria de fazer? Digite: ")
       print("\t1 - Adicionar uma tarefa.")
-      print("\t2 - Listar as tarefas.")
+      print("\t2 - Mostrar as tarefas.")
       print("\t3 - Concluir uma tarefa.")
       print("\t4 - Buscar tarefas na base de dados.")
       print("\t5 - Inserir tarefas na base de dados.")
@@ -20,24 +20,28 @@ class Menu:
 
       if opcoes == 1:
 
-        self.crud.adicionarTarefa(self.listaTarefas)
+        self.crud.adicionaTarefa(self.listaTarefas)
         
         print("\n")
       
       elif opcoes == 2:
-        print("Lista tarefas")
+        self.crud.mostraTarefas(self.listaTarefas)
+
         print("\n")
       
       elif opcoes == 3:
-        print("Concluo tarefas")
+        self.crud.concluiTarefa(self.listaTarefas)
+
         print("\n")
       
       elif opcoes == 4:
-        print("Buscar na base de dados")
+        self.crud.buscaDadosSQLite(self.listaTarefas)
+
         print("\n")
       
       elif opcoes == 5:
-        print("Inserir na base de dados")
+        self.crud.insereDadosSQLite(self.listaTarefas)
+
         print("\n")
       
       elif opcoes == 0:
